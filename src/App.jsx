a529fa20c8c139fd4a904a4578fe11becf4d462c
Cemre Yıldız <100775818+Cemrdeniz@users.noreply.github.com>
2026-05-15 15:00:00 +0300

    feat: add products page and routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
 import Suppliers from "./pages/Suppliers";
@@ -8,10 +9,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/suppliers" element={<Suppliers />} />
-        <Route path="/stock" element={<Stock />} />
+
+        <Route element={<MainLayout />}>
+
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/suppliers" element={<Suppliers />} />
+          <Route path="/stock" element={<Stock />} />
+
+        </Route>
+
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Products.jsx
@@ -0,0 +1,5 @@
+function Products() {
+  return <h1>Ürünler</h1>;
+}
+
+export default Products;
